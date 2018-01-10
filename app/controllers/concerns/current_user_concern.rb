162e654350a33a0d1800a127a001879b0eb6d25f
Brady Berryessa <you@example.com>
2018-01-10 16:52:54 -0700

    Added some petergate functionalities

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -8,8 +8,6 @@ module CurrentUserConcern
   def guest_user
     guest = GuestUser.new
     guest.name = "Guest User"
-    guest.first_name = "Guest"
-    guest.last_name = "User"
     guest.email = "guest@example.com"
     guest
   end
