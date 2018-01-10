a23ed8d100b3ee6f9f8e9aca00bc8e08102b2bd3
Brady Berryessa <you@example.com>
2018-01-10 16:25:19 -0700

    Added petergate some more but I think I screwed a bunch of things up too

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  access all: [:show, :index], user: {except: [:destroy]}, site_admin: :all
 
   # GET /blogs
   # GET /blogs.json
