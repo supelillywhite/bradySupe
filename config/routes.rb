402e5bdc723977fbf34f99062a53b6b62a91126b
Brady Berryessa <you@example.com>
2018-01-11 15:22:12 -0700

    Generated a pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/donate'
+
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :blogs
 
