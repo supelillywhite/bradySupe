62d0703f533348af621a61e6772ea9b68683a6f6
Jerel Lillywhite <supelillywhite@gmail.com>
2018-01-11 17:06:26 -0700

    reverting to where it worked

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -37,7 +37,7 @@ class BlogsController < ApplicationController
   # PATCH/PUT /blogs/1
   # PATCH/PUT /blogs/1.json
   def update
-    respond_to do |format|
+    respond_to do |format| 
       if @blog.update(blog_params)
         format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
         format.json { render :show, status: :ok, location: @blog }
