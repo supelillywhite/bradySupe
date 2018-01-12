88628e8a3ff3084f3b3a4525bca199fea494d331
Brady Berryessa <you@example.com>
2018-01-11 17:30:41 -0700

    Integrated Carrierwave and finally made it work

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,8 @@
 class Blog < ApplicationRecord
   belongs_to :user
+
+  mount_uploader :image, BlogUploader
+
   def self.search(term)
     if term
       titles = Blog.where('LOWER(title) LIKE ?', "%#{term.downcase}%")
